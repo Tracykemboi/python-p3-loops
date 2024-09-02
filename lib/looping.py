8b513d784f77689d7af9366aa5dd56e45276bf41
Tracy Kemboi <tkemboi72@gmail.com>
2024-09-02 11:49:49 +0300

    control flow and looping in python

--- lib/looping.py
+++ lib/looping.py
@@ -14,4 +14,11 @@ def square_integers(int_list):
 
 def fizzbuzz():
     for i in range(1, 101):
-        if 
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
